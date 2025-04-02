11ed54379dfa008bfbf30ad3b220a777a1bf479c
nabin1200 <nabinbasnet1200@gmail.com>
2025-04-02 13:55:15 +0545

    day 3 of learning react

---
+++ react-learning/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import Hero from '../section/herosection' 
+export default function HOmepage(){
+    return(
+        <>
+        <h1>HOmepage</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus velit autem pariatur dolorem ducimus consectetur quis. Officiis cupiditate necessitatibus consequatur tempore voluptas nostrum sint quae dicta deleniti quo, vel omnis?</p>
+        <Hero/>
+        </>
+    )
+
+}
---
+++ react-learning/src/section/herosection.jsx
@@ -0,0 +1,8 @@
+
+const Hero=()=>{
+    return(
+        <h1>this is hero</h1>
+    )
+}
+
+export default  Hero
